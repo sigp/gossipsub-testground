8eaca40500e9d58ae5c952b0144272205de25913
ackintosh <sora.akatsuki@gmail.com>
2023-03-04 13:31:56 +0900

    Add annotation to configmap to create folder structure

--- scripts/eks/monitoring.sh
+++ scripts/eks/monitoring.sh
@@ -9,14 +9,15 @@ create_configmap() {
   # The name of configmap must consist of lower case alphanumeric characters, '-' or '.'
   name="$1_$(basename -s ".json" "$2")"
   name=$(echo "$name" | tr "[:upper:]" "[:lower:]" | tr "_" "-")
-  kubectl create configmap "dashboard-$name" --from-file=$2
+  kubectl create configmap "dashboard-$name" --from-file="$2"
   kubectl label configmap "dashboard-$name" grafana_dashboard=1
+  kubectl annotate configmap "dashboard-$name" grafana_dashboard_folder="$1"
 }
 
 create_configmaps() {
-  find $project_root/$1/dashboards/ -name "*.json" -type f | while read -r fname
+  find "${project_root}/$1/dashboards/" -name "*.json" -type f | while read -r fname
   do
-    create_configmap $1 $fname
+    create_configmap "$1" "${fname}"
   done
 }
 
@@ -30,6 +31,14 @@ create_configmaps "eth_consensus"
 create_configmaps "scoring"
 
 # Install Grafana
+echo -e "Now obtaining the helm charts and installing Grafana. This might take a few minutes.\n"
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install gossipsub-grafana grafana/grafana -f $script_dir/grafana-variables.yaml
+helm install gossipsub-grafana grafana/grafana -f "${script_dir}/grafana-variables.yaml"
+
+echo -e "\n============================================================================================================="
+echo -e "You may run the following in order to obtain the grafana pod name:"
+echo -e 'export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=gossipsub-grafana" -o jsonpath="{.items[0].metadata.name}")\n'
+echo -e "You may now run the following in order to port-forward and access the grafana dashboard from your laptop by opening 'localhost:3000' in your browser:"
+echo -e 'kubectl --namespace default port-forward $POD_NAME 3000'
+echo -e "============================================================================================================\n"
